9e666dd95a31e06d20ce924baa6c7b5e0cb4a0e7
ArshadBeldar <arshadsk9527@gmail.com>
2022-02-20 17:54:58 +0530

    ADDED Simple Program Hello World

---
+++ HELLOWOR.C
@@ -0,0 +1,11 @@
+//			WELCOME TO PROGRAM
+//	Program 1.Simple Program print Hello Wolrd
+// Header File
+#include<stdio.h>
+#include<conio.h>
+void main() //Main Program
+{
+	clrscr();
+	printf("Hello Wolrd");
+	getch();
+}
